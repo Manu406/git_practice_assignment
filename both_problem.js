99d6f607ee458912f7b04d9c32be6ed818b56dd4
manu soda  git config --global user.email you@example.com  git config --global user.name manu <you@example.com>
2022-10-19 17:28:51 +0530

    assignmentjavascript

---
+++ both_problem.js
@@ -0,0 +1,30 @@
+// prime
+
+let num=13;
+let x="Yes";
+for(let i=1;i<num;i++){
+if(num%===0){
+x="No";
+break;
+}
+}
+
+
+
+
+
+
+
+
+  
+// palindrome 
+ let N=6;
+  let str="nrupul"
+ let i=0;
+ let j=N-1;
+ let flag="No";
+while(i<j){
+  if(str[i]!==str[j]){
+  flag="No";
+ 
+}
